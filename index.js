53a7e13210fa1881175d6b0917c3b7a975a7c520
AlineKristina <aline.cristina.2k17@outlook.com>
2022-04-13 15:02:22 -0300

    Adicionado nickname

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-app.get('/', (req, res) => {
+app.use(bodyParser.json());
+app.use(bodyParser({extended: false}));
+
+app.post('/xet', (req, res) => {
+  nickname = req.body.nickname;
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/login', (req, res) => {
+  res.sendFile(__dirname + '/home.html');
+})
+
 io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-      io.emit('chat message', msg);
-    });
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', nickname + ': ' + msg);
   });
+});
 
 server.listen(3000, () => {
   console.log('Listening on PORT 3000');
